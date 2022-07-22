4f26e3a495e9b98ca695e4c969773d59e21e4352
Sebastien Puigrenier <sebastien.puigrenier@gmail.com>
2022-07-22 08:03:32 +0200

    update database.sql

--- backend/database.sql
+++ backend/database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : mer. 20 juil. 2022 à 08:41
+-- Généré le : ven. 22 juil. 2022 à 06:02
 -- Version du serveur : 8.0.28
 -- Version de PHP : 7.4.26
 
@@ -67,8 +67,18 @@ CREATE TABLE IF NOT EXISTS `journees` (
   `id` varchar(100) NOT NULL,
   `numero` int NOT NULL,
   `is_closed` tinyint(1) NOT NULL,
-  PRIMARY KEY (`id`)
+  `nom` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `nom` (`nom`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `journees`
+--
+
+INSERT INTO `journees` (`id`, `numero`, `is_closed`, `nom`) VALUES
+('024f2b9b-edfe-4040-b1a1-1ac23f854ea1', 1, 0, 'Journée 1 Septembre 2021'),
+('389fa67c-29d1-467a-9430-7f03a8c5bd44', 2, 0, 'Journée 2 Octobre 2021');
 
 -- --------------------------------------------------------
 
@@ -78,7 +88,7 @@ CREATE TABLE IF NOT EXISTS `journees` (
 
 DROP TABLE IF EXISTS `matchs`;
 CREATE TABLE IF NOT EXISTS `matchs` (
-  `id` varchar(100) NOT NULL,
+  `id` varchar(300) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `cote_v_equipe_1` decimal(5,2) NOT NULL,
   `cote_v_equipe_2` decimal(5,2) NOT NULL,
   `cote_egalite` decimal(5,2) NOT NULL,
@@ -90,10 +100,27 @@ CREATE TABLE IF NOT EXISTS `matchs` (
   `equipe2_id` varchar(100) DEFAULT NULL,
   `journee_id` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`),
   KEY `matchs_fk1` (`equipe1_id`),
   KEY `matchs_fk2` (`equipe2_id`),
   KEY `matchs_fk3` (`journee_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `matchs`
+--
+
+INSERT INTO `matchs` (`id`, `cote_v_equipe_1`, `cote_v_equipe_2`, `cote_egalite`, `td_equipe_1`, `td_equipe_2`, `sorties_equipe_1`, `sorties_equipe_2`, `equipe1_id`, `equipe2_id`, `journee_id`) VALUES
+('024f2b9b-edfe-4040-b1a1-1ac23f854ea1-089a1ee3-1559-4cea-92e2-40391cd3d043-9f0bddb3-f83e-4d6e-a30b-313f4564f037', '1.45', '4.15', '2.68', NULL, NULL, NULL, NULL, '089a1ee3-1559-4cea-92e2-40391cd3d043', '9f0bddb3-f83e-4d6e-a30b-313f4564f037', '024f2b9b-edfe-4040-b1a1-1ac23f854ea1'),
+('024f2b9b-edfe-4040-b1a1-1ac23f854ea1-404a269d-39da-4498-89ff-bc266d7fbd14-2721ee5a-0a0d-44cf-b18e-4104fc200a54', '3.70', '1.10', '2.45', NULL, NULL, NULL, NULL, '404a269d-39da-4498-89ff-bc266d7fbd14', '2721ee5a-0a0d-44cf-b18e-4104fc200a54', '024f2b9b-edfe-4040-b1a1-1ac23f854ea1'),
+('024f2b9b-edfe-4040-b1a1-1ac23f854ea1-5bf7d04e-fcba-44a8-9789-4a4fc8e60834-d8c2d789-dcf9-4bf0-b62e-f5f4ca1eb7cc', '6.05', '1.55', '3.63', NULL, NULL, NULL, NULL, '5bf7d04e-fcba-44a8-9789-4a4fc8e60834', 'd8c2d789-dcf9-4bf0-b62e-f5f4ca1eb7cc', '024f2b9b-edfe-4040-b1a1-1ac23f854ea1'),
+('024f2b9b-edfe-4040-b1a1-1ac23f854ea1-acbb7f04-f4e2-48f6-af82-ccdd09661c4f-c9d61256-4e86-4b29-bc07-64e09d87eddf', '1.25', '1.35', '1.90', NULL, NULL, NULL, NULL, 'acbb7f04-f4e2-48f6-af82-ccdd09661c4f', 'c9d61256-4e86-4b29-bc07-64e09d87eddf', '024f2b9b-edfe-4040-b1a1-1ac23f854ea1'),
+('024f2b9b-edfe-4040-b1a1-1ac23f854ea1-e71325ab-a6de-424f-b5cb-4351fb24b165-ad97169d-a6dd-47e9-ae6b-0fae5f6194b8', '2.20', '2.40', '2.90', NULL, NULL, NULL, NULL, 'e71325ab-a6de-424f-b5cb-4351fb24b165', 'ad97169d-a6dd-47e9-ae6b-0fae5f6194b8', '024f2b9b-edfe-4040-b1a1-1ac23f854ea1'),
+('389fa67c-29d1-467a-9430-7f03a8c5bd44-404a269d-39da-4498-89ff-bc266d7fbd14-5bf7d04e-fcba-44a8-9789-4a4fc8e60834', '2.60', '7.10', '4.15', NULL, NULL, NULL, NULL, '404a269d-39da-4498-89ff-bc266d7fbd14', '5bf7d04e-fcba-44a8-9789-4a4fc8e60834', '389fa67c-29d1-467a-9430-7f03a8c5bd44'),
+('389fa67c-29d1-467a-9430-7f03a8c5bd44-acbb7f04-f4e2-48f6-af82-ccdd09661c4f-2721ee5a-0a0d-44cf-b18e-4104fc200a54', '1.25', '1.10', '1.78', NULL, NULL, NULL, NULL, 'acbb7f04-f4e2-48f6-af82-ccdd09661c4f', '2721ee5a-0a0d-44cf-b18e-4104fc200a54', '389fa67c-29d1-467a-9430-7f03a8c5bd44'),
+('389fa67c-29d1-467a-9430-7f03a8c5bd44-ad97169d-a6dd-47e9-ae6b-0fae5f6194b8-d8c2d789-dcf9-4bf0-b62e-f5f4ca1eb7cc', '3.95', '1.55', '2.58', NULL, NULL, NULL, NULL, 'ad97169d-a6dd-47e9-ae6b-0fae5f6194b8', 'd8c2d789-dcf9-4bf0-b62e-f5f4ca1eb7cc', '389fa67c-29d1-467a-9430-7f03a8c5bd44'),
+('389fa67c-29d1-467a-9430-7f03a8c5bd44-c9d61256-4e86-4b29-bc07-64e09d87eddf-9f0bddb3-f83e-4d6e-a30b-313f4564f037', '1.35', '4.05', '2.63', NULL, NULL, NULL, NULL, 'c9d61256-4e86-4b29-bc07-64e09d87eddf', '9f0bddb3-f83e-4d6e-a30b-313f4564f037', '389fa67c-29d1-467a-9430-7f03a8c5bd44'),
+('389fa67c-29d1-467a-9430-7f03a8c5bd44-e71325ab-a6de-424f-b5cb-4351fb24b165-089a1ee3-1559-4cea-92e2-40391cd3d043', '3.65', '1.45', '2.43', NULL, NULL, NULL, NULL, 'e71325ab-a6de-424f-b5cb-4351fb24b165', '089a1ee3-1559-4cea-92e2-40391cd3d043', '389fa67c-29d1-467a-9430-7f03a8c5bd44');
 
 -- --------------------------------------------------------
 
@@ -104,7 +131,7 @@ CREATE TABLE IF NOT EXISTS `matchs` (
 DROP TABLE IF EXISTS `paris`;
 CREATE TABLE IF NOT EXISTS `paris` (
   `user_id` varchar(100) NOT NULL,
-  `match_id` varchar(100) NOT NULL,
+  `match_id` varchar(300) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `somme` int NOT NULL,
   `pari_victoire_e1` tinyint(1) NOT NULL,
   `pari_victoire_e2` tinyint(1) NOT NULL,
@@ -112,6 +139,22 @@ CREATE TABLE IF NOT EXISTS `paris` (
   KEY `paris_fk1` (`user_id`),
   KEY `paris_fk2` (`match_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `paris`
+--
+
+INSERT INTO `paris` (`user_id`, `match_id`, `somme`, `pari_victoire_e1`, `pari_victoire_e2`, `pari_egalite`) VALUES
+('49a67d32-3ad7-429d-81c0-22a0edffd658', '024f2b9b-edfe-4040-b1a1-1ac23f854ea1-089a1ee3-1559-4cea-92e2-40391cd3d043-9f0bddb3-f83e-4d6e-a30b-313f4564f037', 1500, 1, 0, 0),
+('49a67d32-3ad7-429d-81c0-22a0edffd658', '024f2b9b-edfe-4040-b1a1-1ac23f854ea1-404a269d-39da-4498-89ff-bc266d7fbd14-2721ee5a-0a0d-44cf-b18e-4104fc200a54', 1000, 0, 0, 1),
+('49a67d32-3ad7-429d-81c0-22a0edffd658', '024f2b9b-edfe-4040-b1a1-1ac23f854ea1-5bf7d04e-fcba-44a8-9789-4a4fc8e60834-d8c2d789-dcf9-4bf0-b62e-f5f4ca1eb7cc', 999, 1, 0, 0),
+('49a67d32-3ad7-429d-81c0-22a0edffd658', '024f2b9b-edfe-4040-b1a1-1ac23f854ea1-acbb7f04-f4e2-48f6-af82-ccdd09661c4f-c9d61256-4e86-4b29-bc07-64e09d87eddf', 4500, 1, 0, 0),
+('49a67d32-3ad7-429d-81c0-22a0edffd658', '024f2b9b-edfe-4040-b1a1-1ac23f854ea1-e71325ab-a6de-424f-b5cb-4351fb24b165-ad97169d-a6dd-47e9-ae6b-0fae5f6194b8', 1200, 0, 0, 1),
+('5fe32708-0328-412f-a150-344c41da2056', '024f2b9b-edfe-4040-b1a1-1ac23f854ea1-089a1ee3-1559-4cea-92e2-40391cd3d043-9f0bddb3-f83e-4d6e-a30b-313f4564f037', 1000, 1, 0, 0),
+('5fe32708-0328-412f-a150-344c41da2056', '024f2b9b-edfe-4040-b1a1-1ac23f854ea1-e71325ab-a6de-424f-b5cb-4351fb24b165-ad97169d-a6dd-47e9-ae6b-0fae5f6194b8', 1000, 0, 0, 1),
+('5fe32708-0328-412f-a150-344c41da2056', '024f2b9b-edfe-4040-b1a1-1ac23f854ea1-404a269d-39da-4498-89ff-bc266d7fbd14-2721ee5a-0a0d-44cf-b18e-4104fc200a54', 1200, 0, 0, 1),
+('5fe32708-0328-412f-a150-344c41da2056', '024f2b9b-edfe-4040-b1a1-1ac23f854ea1-5bf7d04e-fcba-44a8-9789-4a4fc8e60834-d8c2d789-dcf9-4bf0-b62e-f5f4ca1eb7cc', 4500, 1, 0, 0),
+('5fe32708-0328-412f-a150-344c41da2056', '024f2b9b-edfe-4040-b1a1-1ac23f854ea1-acbb7f04-f4e2-48f6-af82-ccdd09661c4f-c9d61256-4e86-4b29-bc07-64e09d87eddf', 7500, 1, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -137,6 +180,8 @@ CREATE TABLE IF NOT EXISTS `users` (
 
 INSERT INTO `users` (`id`, `pseudo`, `email`, `avatar`, `password`, `isadmin`) VALUES
 ('367974e2-4fc7-400e-aa2b-b56e35263d67', 'Nithog', 'nithog@gmail.com', NULL, '$2b$10$ZKj7uLyj67y0P3KgVuonU.7J1dNcIS8//oBt3hLSCAfJhdg6.2MTK', 0),
+('49a67d32-3ad7-429d-81c0-22a0edffd658', 'Anthony', 'anthony.G@wcs.com', NULL, '$2b$10$BB169wh4ksuRbvNCdssSHOen9hte3jVYpcSL.owJKv3za/upimCOG', 0),
+('5fe32708-0328-412f-a150-344c41da2056', 'Kirdom', 'kirdom@hotmail.fr', 'a58e855864a528b229eacf100.jpg', '$2b$10$m3J2UZwocHjENqJ3koPIyeSccArbXrJyhdQMaeVKT8kHRnluDmqzS', 0),
 ('763b3115-5308-430d-98d7-706504c64792', 'Marauth', 'sebastien.puigrenier@gmail.com', NULL, '$2b$10$xzOYawmjOvpxbkpqZnLexeGRlHB.WOvDVfQkI5HhP9ICG0QYf0rE.', 1),
 ('c48ffa6d-610e-4474-ae8d-2d8a300cc34f', 'Mordikar', 'mordikar@gmail.com', NULL, '$2b$10$qJAcNEUC4eENRUP1ISaBRuv8gZmGmDZiJArS1MTlooHp6DPHNGYc6', 0);
 
